f225aefa1e7057104b08f6f9298f86aea2164816
Артём <zolotoyq@gmail.com>
2020-12-10 17:00:00 +0300

    more tests()

--- src/main/SKipList.kt
+++ src/main/SKipList.kt
@@ -1,10 +1,14 @@
+import java.util.*
 import kotlin.random.Random
 
-class SKipList {
+class SKipList : SortedSet<Int> {
     private var head = SkipNode(0, 33)
     private var levels = 1
 
-    fun add(value: Int) {
+    override val size: Int
+        get() = sizeForAllLevel()
+
+    override fun add(element: Int): Boolean {
         var level = 0
         var random = Random.nextInt()
         while (random and 1 == 1) {
@@ -15,11 +19,11 @@ class SKipList {
             }
             random = random shr 1
         }
-        val node = SkipNode(value, level + 1)
+        val node = SkipNode(element, level + 1)
         var cur = head
         for (i in levels - 1 downTo 0) {
             while (cur.next[i] != null) {
-                if (cur.next[i]!!.value > value) {
+                if (cur.next[i]!!.value > element) {
                     break
                 }
                 cur = cur.next[i]!!
@@ -29,21 +33,22 @@ class SKipList {
                 cur.next[i] = node
             }
         }
-
+        return true
     }
 
-    fun addAll(collection: Collection<Int>) {
-        for (element in collection)
+    override fun addAll(elements: Collection<Int>): Boolean {
+        for (element in elements)
             add(element)
+        return true
     }
 
-    fun contains(value: Int): Boolean {
+    override fun contains(element: Int): Boolean {
         var cur = head
         for (i in levels - 1 downTo 0) {
             while (cur.next[i] != null) {
-                if (cur.next[i]?.value == value)
+                if (cur.next[i]?.value == element)
                     return true
-                if (cur.next[i]?.value!! > value)
+                if (cur.next[i]?.value!! > element)
                     break
                 cur = cur.next[i]!!
             }
@@ -51,23 +56,70 @@ class SKipList {
         return false
     }
 
-    fun isEmpty(): Boolean {
+    override fun containsAll(elements: Collection<Int>): Boolean {
+        for (element in elements)
+            if (!contains(element))
+                return false
+        return true
+    }
+
+    override fun isEmpty(): Boolean {
         return head.next[0] == null
     }
 
-    fun remove(value: Int) {
+    override fun remove(element: Int): Boolean {
         var cur = head
+        var found = false
         for (i in levels - 1 downTo 0) {
             while (cur.next[i] != null) {
-                if (cur.next[i]?.value == value) {
+                if (cur.next[i]?.value == element) {
+                    found = true
                     cur.next[i] = cur.next[i]?.next?.get(i)
                     break
                 }
-                if (cur.next[i]?.value!! > value)
+                if (cur.next[i]?.value!! > element)
                     break
                 cur = cur.next[i]!!
             }
         }
+        return found
+    }
+
+    override fun removeAll(elements: Collection<Int>): Boolean {
+        for (element in elements)
+            if (!remove(element))
+                return false
+        return true
+    }
+
+    override fun retainAll(elements: Collection<Int>): Boolean {
+        for (i in levels - 1 downTo 0) {
+            var cur = head
+            while (cur.next[i] != null) {
+                if (!elements.contains(cur.next[i]?.value))
+                    cur.next[i] = cur.next[i]?.next?.get(i)
+                else
+                    cur = cur.next[i]!!
+            }
+        }
+        for (i in levels - 1 downTo 0)
+            if (levelSize(i) == 0 || (levelSize(i) == 1 && levelSize(i - 1) == 1))
+                levels--
+        return true
+    }
+
+    override fun clear() {
+        for (i in levels - 1 downTo 0) {
+            var cur = head
+            while (cur.next[i] != null) {
+                cur.next[i] = null
+                if (cur.next[i]?.next?.get(i) == null)
+                    break
+                else
+                    cur = cur.next[i]!!
+            }
+        }
+        levels = 1
     }
 
     fun levelSize(level: Int): Int {
@@ -78,6 +130,66 @@ class SKipList {
             cur = cur.next[level]!!
         }
         return size
+    }
+
+    private fun sizeForAllLevel(): Int {
+        var size = 0
+        for (i in levels - 1 downTo 0) {
+            size += levelSize(i)
+        }
+        return size
+    }
+
+    override fun first(): Int? {
+        return head.next[0]?.value
+    }
+
+    override fun last(): Int? {
+        var cur = head
+        while (cur.next[0]?.next?.get(0) != null)
+            cur = cur.next[0]!!
+        return cur.next[0]?.value
+    }
+
+    override fun tailSet(fromElement: Int): SKipList {
+        val sl = SKipList()
+        var cur = head
+        while (cur.next[0] != null) {
+            if (cur.next[0]!!.value > fromElement)
+                sl.add(cur.next[0]!!.value)
+            cur = cur.next[0]!!
+        }
+        return sl
+    }
+
+    override fun headSet(toElement: Int): SKipList {
+        val sl = SKipList()
+        var cur = head
+        while (cur.next[0] != null) {
+            if (cur.next[0]!!.value < toElement)
+                sl.add(cur.next[0]!!.value)
+            cur = cur.next[0]!!
+        }
+        return sl
+    }
+
+    override fun subSet(fromElement: Int, toElement: Int): SKipList {
+        val sl = SKipList()
+        var cur = head
+        while (cur.next[0] != null) {
+            if (cur.next[0]!!.value in (fromElement + 1) until toElement)
+                sl.add(cur.next[0]!!.value)
+            cur = cur.next[0]!!
+        }
+        return sl
+    }
+
+    override fun iterator(): MutableIterator<Int> {
+        TODO("Not yet implemented")
+    }
+
+    override fun comparator(): Comparator<in Int>? {
+        TODO("Not yet implemented")
     }
 
     fun printSkipList() {
@@ -93,3 +205,4 @@ class SKipList {
         }
     }
 }
+
--- src/test/java/SkipListTest.kt
+++ src/test/java/SkipListTest.kt
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Assertions.*
 class SkipListTest {
 
     @Test
-    fun print() {
+    fun add() {
         val test = SKipList()
         test.add(16)
         test.add(2)
         test.add(4)
-        test.addAll(listOf(49,54,22,5))
+        test.addAll(listOf(49, 54, 22, 5))
         test.printSkipList()
     }
 
@@ -25,29 +25,70 @@ class SkipListTest {
     fun contains() {
         val test = SKipList()
         assertFalse(test.contains(190))
-        test.add(190)
+        test.addAll(setOf(190, 55, 12, 43, 22, 9))
         assertTrue(test.contains(190))
+        assertTrue(test.containsAll(listOf(190, 9, 12)))
+        assertFalse(test.containsAll(listOf(55, 2, 43)))
     }
 
     @Test
     fun remove() {
         val test = SKipList()
-        test.addAll(setOf(46,12,21,65,88,1221,8898,232))
+        test.addAll(setOf(46, 12, 21, 65, 88, 1221, 8898, 232))
         test.printSkipList()
         println("------")
         test.remove(65)
-        test.remove(9)
+        test.removeAll(listOf(9, 21, 8898))
         test.printSkipList()
     }
 
     @Test
     fun size() {
         val test = SKipList()
-        test.addAll(setOf(17,22,24,48,51,69,19))
+        test.addAll(setOf(17, 22, 24, 48, 51, 69, 19))
         test.printSkipList()
         println(test.levelSize(1))
         println(test.levelSize(10))
     }
 
+    @Test
+    fun clear() {
+        val test = SKipList()
+        test.add(32)
+        assertFalse(test.isEmpty())
+        test.clear()
+        assertTrue(test.isEmpty())
+    }
+
+    @Test
+    fun firstAndLast() {
+        val test = SKipList()
+        assertEquals(null, test.first())
+        assertEquals(null, test.last())
+        test.addAll(setOf(45, 2, 36, 16))
+        assertEquals(2, test.first())
+        assertEquals(45, test.last())
+    }
+
+    @Test
+    fun sets() {
+        val test = SKipList()
+        test.addAll(setOf(12, 42, 55, 12, 222, 36))
+        test.tailSet(40).printSkipList()
+        println("-----")
+        test.headSet(55).printSkipList()
+        println("-----")
+        test.subSet(20, 100).printSkipList()
+    }
+
+    @Test
+    fun retain() {
+        val test = SKipList()
+        test.addAll(setOf(42,12,5,675,124,879,1345,11,5542,1))
+        test.printSkipList()
+        println("-----")
+        test.retainAll(setOf(124,5,675,1))
+        test.printSkipList()
+    }
 }
 
